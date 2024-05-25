cc02813a657173da5e33630ec63a4db5471db6a3
EremK <105379532+EremK@users.noreply.github.com>
2024-05-26 00:02:44 +0200

    Done: 1) User can specify the board size, nickname. 2) The board size can be between 10x10 and 25x25. 3) Added temporary code to test correctness of game-window updating. 4) Initial frontend is added.

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+import threading
+import random
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+app.secret_key = 'qwerty'
+
+game_board = []
+board_lock = threading.Lock()
+update_thread = None
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def validate_size(size):
+    try:
+        size = int(size)
+        if 10 <= size <= 25:
+            return size
+        else:
+            return 20
+    except ValueError:
+        return 20
 
 
-# Press the green button in the gutter to run the script.
+def update_board():
+    global game_board
+    while True:
+        time.sleep(1)
+        # TEMPORARY. Thread test
+        with board_lock:
+            for row in game_board:
+                for i in range(len(row)):
+                    row[i] = 0
+            i = random.randint(0, len(game_board) - 1)
+            j = random.randint(0, len(game_board[0]) - 1)
+            game_board[i][j] = 1
+        # TEMPORARY. Thread test
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        size = validate_size(request.form['size'])
+        session['player_name'] = request.form['name']
+        session['size'] = size
+        return redirect(url_for('board'))
+    return render_template('index.html')
+
+
+@app.route('/board', methods=['GET'])
+def board():
+    size = session.get('size', 20)
+    global game_board
+    game_board = [[0 for _ in range(size)] for _ in range(size)]
+    return render_template('board.html', size=size, name=session.get('player_name'))
+
+
+@app.route('/start_game', methods=['POST'])
+def start_game():
+    global update_thread
+    if update_thread is None:
+        update_thread = threading.Thread(target=update_board)
+        update_thread.daemon = True
+        update_thread.start()
+    return jsonify({'status': 'started'})
+
+
+@app.route('/get_board')
+def get_board():
+    with board_lock:
+        return jsonify(game_board)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
